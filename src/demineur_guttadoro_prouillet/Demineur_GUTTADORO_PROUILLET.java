8ada2babf0fa15dbdda9db02050d3a1edc298b4c
Lorraine <Lorraine@DESKTOP-52IMI0I.home>
2020-11-24 09:17:15 +0100

    lol

--- src/demineur_guttadoro_prouillet/Demineur_GUTTADORO_PROUILLET.java
+++ src/demineur_guttadoro_prouillet/Demineur_GUTTADORO_PROUILLET.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 package demineur_guttadoro_prouillet;
 
@@ -16,6 +14,7 @@ public class Demineur_GUTTADORO_PROUILLET {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
