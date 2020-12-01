fa2b19804d9f3cab15b62e6b19325c9f994fc437
Rosanna Guttadoro <Rosanna Guttadoro@LAPTOP-DJ9N1RIH>
2020-12-01 15:53:19 +0100

    classes

---
+++ src/demineur_guttadoro_prouillet/Joueur.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demineur_guttadoro_prouillet;
+
+/**
+ *
+ * @author Rosanna Guttadoro
+ */
+public class Joueur {
+    
+    // def des attributs:
+    String Nom;
+    double tempsJoueur;
+    
+    public Joueur () { //constructeur
+        
+    }
+    
+}
