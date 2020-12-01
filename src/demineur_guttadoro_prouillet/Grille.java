18f3f0b60208e868454fb13e912baee3a2970bce
Lorraine <Lorraine@DESKTOP-52IMI0I.home>
2020-12-01 17:22:14 +0100

    grille

--- src/demineur_guttadoro_prouillet/Grille.java
+++ src/demineur_guttadoro_prouillet/Grille.java
@@ -21,5 +21,79 @@ public class Grille {
         }
     }
     
-    
+    public void afficherGrilleSurConcole () {
+        // Pour l'affichage on parcours le tableau dans le sens inverse des lignes
+        for ( int i=5 ; i>= 0 ; i--) {
+            //En commencant par la ligne 5, puis 4 ect...
+            for ( int j=0 ; j<=6 ; j++ ) {
+               
+                if  ( Cellules[i][j].jetonCourant == null ) {
+                    if ( Cellules[i][j].trouNoir == true) {
+                        System.out.print("T ");
+                    // T corresponds à un trou noir
+                    }else if ( Cellules[i][j].desintegrateur == true) {
+                        System.out.print("D ");
+                    // D corresponds a un désintégrateur
+                    } else {
+                        System.out.print("V ");
+                    //V corresponds à  case vide
+                    }
+                } else {
+                    
+                    if (Cellules[i][j].jetonCourant.lireCouleur() == "rouge") {
+                        System.out.print("R " );      
+                    }
+                    if (Cellules[i][j].jetonCourant.lireCouleur() == "jaune") {
+                        System.out.print("J " );
+                    }
+                    
+                 
+                  // Si c'est aucune des précédentes cases, c'est donc forcément un jeton
+                  // jetonCourant est de type jeton et un jeton a pour attribut une couleur
+                } //(On lit sa couleur est affiche R ou J en fonction)
+                if (j==6) {
+                      System.out.print("\n");
+                }
+            }
+        }
+        
+    }
+        
+        public void initialiser_mines_en_contact () { 
+        for (int i=1;i<5;i++)
+        {
+            for(int j=1;j<5;j++)
+            {
+                if(cellules[i-1][j-1].presenceMines()==true) {
+                    cellules[i][j].mines_en_contact++;
+                }
+                if(cellules[i][j-1].presenceMines()==true) { 
+                    cellules[i][j].mines_en_contact++;
+                }
+   
+                if(cellules[i][j+1].presenceMines()==true ) {
+                    cellules[i][j].mines_en_contact++;
+                }
+                if(cellules[i+1][j-1].presenceMines()==true){
+                    cellules[i][j].mines_en_contact++;
+                }
+                if(cellules[i-1][j].presenceMines()==true){
+                    cellules[i][j].mines_en_contact++;
+                }
+                if(cellules[i+1][j].presenceMines()==true){
+                    cellules[i][j].mines_en_contact++;
+                }
+                if(cellules[i+1][j+1].presenceMines()==true){
+                    cellules[i][j].mines_en_contact++;
+                }
+                if(cellules[i-1][j+1].presenceMines()==true){
+                    cellules[i][j].mines_en_contact++;
+                }
+                
+            }
+        }
+    }
+        
+        
+        
 }
