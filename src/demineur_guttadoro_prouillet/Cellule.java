124e44381e095cc167c3189dfeb1c66e8f24493e
Lorraine <Lorraine@DESKTOP-52IMI0I.home>
2020-12-01 16:15:45 +0100

    cellule

--- src/demineur_guttadoro_prouillet/Cellule.java
+++ src/demineur_guttadoro_prouillet/Cellule.java
@@ -10,5 +10,50 @@ package demineur_guttadoro_prouillet;
  * @author Rosanna Guttadoro
  */
 public class Cellule {
+
+
+    //Attributs
+    
+    boolean Kits;
+    boolean Mines;
+    int mines_en_contact;
+    
+
+    //Constructeur
+    public Cellule( ) { // initialisation de la cellule sans kits ni mines 
+        
+        Kits = false;
+        Mines=false;
+        mines_en_contact = 0 ;
+    }
+    
+    public boolean presenceKits () {
+        if ( Kits == true ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean presenceMines () {
+        if ( Mines == true ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+       
+    public boolean recupererKits () {
+        //On tombe sur un kit et désire le récupérer
+        if ( Kits == true ) { //S'il y en a un sur la case, booléen vrai
+            Kits=false;  //On l'enlève et le booléen devient faux
+            return true; // Renvoie vrai, il a bien était retiré
+        } else {
+            return false; //Aucun kits présent , renvoie faux.
+        }
+    }
+        
     
 }
+
+   
